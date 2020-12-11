fb1854a58b9e92835dd8cba3f38b6b239b3dd205
Abhishek Chakraborty <abhishek@Datas-MacBook-Pro-2.local>
2020-12-11 18:23:24 -0500

    main: Committing very first app setup, modified from npx create-react-app

---
+++ web-server/react-app/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Welcome to Mapnificent!
+      </header>
+    </div>
+  );
+}
+
+export default App;
